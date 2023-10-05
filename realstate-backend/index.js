12678da65ba0b7264bd1441ac42d4187dba89bd6
Israel Getahun <israelgetahun20@gmail.com>
2023-10-05 15:10:41 +0300

    create test api route

--- realstate-backend/index.js
+++ realstate-backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -12,6 +13,10 @@ mongoose
     console.log(err);
   });
 const app = express();
+
+app.use("/api/user", userRouter);
+// app.use('/api/auth', authRouter);
+// app.use('/api/listing', listingRouter);
 
 app.listen(3000, () => {
   console.log("server is running on port 300");
