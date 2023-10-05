7a7f98e3bcd7ad97fdb6177bd7af1b8a7c43cecf
Israel Getahun <israelgetahun20@gmail.com>
2023-10-05 16:05:57 +0300

    create signup api route

--- realstate-backend/index.js
+++ realstate-backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -14,8 +15,10 @@ mongoose
   });
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/user", userRouter);
-// app.use('/api/auth', authRouter);
+app.use("/api/auth", authRouter);
 // app.use('/api/listing', listingRouter);
 
 app.listen(3000, () => {
