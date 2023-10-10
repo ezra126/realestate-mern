3d3cfb11c2402539a312c4c8d09a3e730d3ab0dd
Israel Getahun <israelgetahun20@gmail.com>
2023-10-10 11:13:31 +0300

    create update listing functionality

--- realstate/src/App.jsx
+++ realstate/src/App.jsx
@@ -4,6 +4,7 @@ import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 import About from "./pages/About";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
@@ -29,10 +30,10 @@ function App() {
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
-          {/* <Route
+          <Route
             path="/update-listing/:listingId"
             element={<UpdateListing />}
-          /> */}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
